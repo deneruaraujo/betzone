539edf7e439bc026ed08adca37baf5ffbb2264eb
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2024-07-07 16:56:27 -0300

    Fix: Presenter now return all data

--- betzone-back-end/src/infra/http/presenters/activity-presenter.ts
+++ betzone-back-end/src/infra/http/presenters/activity-presenter.ts
@@ -8,6 +8,8 @@ export class ActivityPresenter {
       description: activity.description,
       status: activity.status,
       category: activity.category,
+      createdAt: activity.createdAt,
+      updatedAt: activity.updatedAt,
     };
   }
 }
